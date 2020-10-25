f7d4204ef4045e02d44c9f3994a2dc242feb46f7
Yoojin oh <funluvin@naver.com>
2020-10-25 13:40:33 +0900

    test

--- websocket1/Program.cs
+++ websocket1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperSocket.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,33 @@ namespace websocket1
 {
     class Program
     {
+        private static WebSocketServer wsServer;
+
         static void Main(string[] args)
         {
+            wsServer = new WebSocketServer();
+            int port = 8801;
+            wsServer.Setup(port);
+
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            throw new NotImplementedException();
         }
     }
 }
